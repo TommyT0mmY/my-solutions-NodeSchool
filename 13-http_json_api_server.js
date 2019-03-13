be333eb9c6b0e91f79db263e71c37abc54d4c74e
Tommaso Goni <tommasogo99@gmail.com>
2019-03-13 19:47:54 +0100

    Updated solution to problem 13

--- 13-http_json_api_server.js
+++ 13-http_json_api_server.js
@@ -1,3 +1,7 @@
+/*
+Run me with: node <filename> <listening port>
+*/
+
 const http = require("http");
 const port = Number(process.argv[2]); //first argument
 
@@ -21,11 +25,14 @@ var server = http.createServer(function (request, response) {
      const parsetime_json = JSON.stringify({ hour: Number(zeroFill(date.getHours())), minute: Number(zeroFill(date.getMinutes())), second: Number(zeroFill(date.getSeconds())) });
      response.end(parsetime_json.toString());
    }
-   else { // '/api/unixtime'
+   else if (url.pathname === "/api/unixtime") {
      const iso = url.path.replace("/api/unixtime?iso=","");
      const date = new Date(iso);
      const unixtime_json = JSON.stringify({ unixtime: date.getTime()});
      response.end(unixtime_json.toString());
+   }
+   else {
+     response.end("Send GET request\nTo parse an ISO Date format => http://.../api/parsetime?iso=2019-03-13T17:40Z\nTo get the UNIX timestamp value from an ISO Date format => http://.../api/unixtime?iso=2019-03-13T17:40Z");
    }
 });
 
